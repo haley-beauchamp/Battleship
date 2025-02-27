4a657b03d81a34c1a9ee7afa1cbcf98ebda9adc5
haley-beauchamp <hbeauchamp04@gmail.com>
2025-02-27 15:27:44 -0600

    Enemy Can Now Attack, Attack Logic Improved
    Enemy now attacks randomly. Neither the enemy nor the player can make repeat moves. There is now a win condition.

--- src/battleship.js
+++ src/battleship.js
@@ -13,20 +13,80 @@ enemy.game_board.placeShip(2, ["A1", "A2"]);
 const enemy_board = document.getElementById("enemy-board");
 createBoard(enemy_board, attack, true);
 
+let turn = "player";
+
 function attack(coordinate) {
-    console.log("Attack at:", coordinate);
-    enemy.game_board.receiveAttack(coordinate);
-    const shot = document.querySelector(`#enemy-board .cell[data-coordinate="${coordinate}"]`);
+	if (turn === "player") {
+		const response = enemy.game_board.receiveAttack(coordinate);
+		if (response != "Try Again") {
+			const shot = document.querySelector(`#enemy-board .cell[data-coordinate="${coordinate}"]`);
 
-    switch (enemy.game_board.getCoordinateStatus(coordinate)) {
-        case "Hit":
-            shot.classList.add("shot-hit");
-            break;
-        case "Missed":
-            shot.classList.add("shot-missed");
-            break;
-        default:
-            break;
-    }
+			switch (enemy.game_board.getCoordinateStatus(coordinate)) {
+				case "Hit":
+					shot.classList.add("shot-hit");
+					break;
+				case "Missed":
+					shot.classList.add("shot-missed");
+					break;
+				default:
+					break;
+			}
 
+			if (response === "All Ships Sunk") {
+				gameOver();
+				return;
+			}
+
+			switchTurn();
+		}
+	}
+}
+
+function enemyAttack() {
+	while (true) {
+		const letter = String.fromCharCode(65 + Math.floor(Math.random() * 10));
+		const number = Math.floor(Math.random() * 10) + 1;
+		const coordinate = `${letter}${number}`;
+
+		const response = player.game_board.receiveAttack(coordinate);
+		if (response != "Try Again") {
+			const shot = document.querySelector(`#player-board .cell[data-coordinate="${coordinate}"]`);
+
+			switch (player.game_board.getCoordinateStatus(coordinate)) {
+				case "Hit":
+					shot.classList.add("shot-hit");
+					break;
+				case "Missed":
+					shot.classList.add("shot-missed");
+					break;
+				default:
+					break;
+			}
+
+			if (response === "All Ships Sunk") {
+				gameOver();
+				return;
+			}
+
+			switchTurn();
+			return;
+		}
+	}
+}
+
+function gameOver() {
+	alert(`${turn} Wins!`);
+    
+	const cells = document.querySelectorAll(".cell");
+	cells.forEach((cell) => {
+		cell.classList.add("disabled");
+	});
+}
+
+function switchTurn() {
+	turn = turn === "player" ? "enemy" : "player";
+
+	if (turn === "enemy") {
+		enemyAttack();
+	}
 }
